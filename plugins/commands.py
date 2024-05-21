a5fdcafb2dba01a670a01cccb87f8489184055c3
Divytech <155970643+Divytech@users.noreply.github.com>
2024-05-21 16:45:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -261,7 +261,7 @@ async def start(client, message):
         protect_content=True if pre == 'filep' else False,
     )
     filesarr.append(msg)
-    k = await msg.reply(chat_id = message.from_user.id, text=f"<b><u>❗️❗️ATTENTION❗️️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>5 Minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this ALL Files/Videos to your Saved Messages and Start Download there\n\nHOW TO SAVE - https://t.me/howtodevbots/2</i></b>")
+    k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️ATTENTION❗️️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>5 Minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this ALL Files/Videos to your Saved Messages and Start Download there\n\nHOW TO SAVE - https://t.me/howtodevbots/2</i></b>")
     await asyncio.sleep(300)
     for x in filesarr:
         await x.delete()
